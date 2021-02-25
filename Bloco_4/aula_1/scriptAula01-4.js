a2a63d8478dc289b182a63d636c0d17f307d14bb
igson <igson660@gmail.com>
2021-02-25 13:53:18 -0500

    aula 4.1

---
+++ Bloco_4/aula_1/scriptAula01-4.js
@@ -0,0 +1,19 @@
+
+let estado = "aprovado";
+
+switch (estado) {
+    case "aprovado":
+        console.log("Parabéns você foi aprovado(a)")
+        break;
+
+    case "lista":
+        console.log("Você esta na nossa lista de espera")
+        break;
+
+    case "reprovdado":
+        console.log("Você foi reprovado(a)")
+        break;
+
+    default:
+    console.log("nda");
+}
