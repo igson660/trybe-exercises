62cfbee8093a1084cdfdbfd58e2229d64681a2da
igson <igson660@gmail.com>
2021-02-26 16:36:35 -0500

    aula 02 incompleta

---
+++ Bloco_4/aula_2/scriptAula01.js
@@ -0,0 +1,52 @@
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// console.log(tasksList.length);
+
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// let searchForFirstTask = tasksList[0];
+// console.log(searchForFirstTask);
+
+
+// let searchForLastTask = tasksList[tasksList.length - 1];
+// console.log(searchForLastTask);
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// tasksList.push('Fazer exercícios da Trybe');  // adiciona mais uma tarefa
+// console.log(tasksList);
+
+// ['Tomar café', 'Reunião', 'Brincar com o cachorro', 'Fazer exercícios da Trybe']
+
+// let tasksList = ['Tomar café', 'Reunião', 'Brincar com o cachorro'];
+
+// tasksList.pop();  // remove a última tarefa
+// console.log(tasksList);
+
+// [ 'Tomar café', 'Reunião' ]
+
+// //Atividade 01 obter avlor
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu[1];
+
+// console.log(menuServices);
+
+//Atividade 02
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu.indexOf("Portfólio");
+
+console.log(indexOfPortfolio);
+
+//Atividade 03
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu.push("contato");
+
+console.log(menu);
+
+
+
+
+
+
+
